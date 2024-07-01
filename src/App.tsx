fc101331631ac4734a2c116d75d1c250ce41df46
fernando <fer.ema.gonzalez@gmail.com>
2024-07-01 18:18:23 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './Components/Login/Login';
-import authService from './Components/AuthServices';
-import { HeaderMegaMenu } from './Components/HeaderMegaMenu/HeaderMegaMenu';
 import PublicComponent from './Components/PublicComponent/PublicComponent';
-import { NavbarSearch } from './Components/Admin/Admin';
-import { MantineProvider } from '@mantine/core';
 
 const App: React.FC = () => {
   return (
