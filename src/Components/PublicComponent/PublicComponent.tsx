527540b3f0ca1602879f1cb14c343fc43fed7040
fernando <fer.ema.gonzalez@gmail.com>
2024-07-03 15:37:44 -0300

    quito estilos al componente y los paso a App.tsx

--- src/Components/PublicComponent/PublicComponent.tsx
+++ src/Components/PublicComponent/PublicComponent.tsx
@@ -1,7 +1,5 @@
-import { MantineProvider } from "@mantine/core"
 import { Certificaciones } from "../Certificaciones/Certificaciones"
 import { Contacto } from "../Contacto/Contacto"
-import { FooterSocial } from "../FooterSocial/FooterSocial"
 import { HeaderMegaMenu } from "../HeaderMegaMenu/HeaderMegaMenu"
 import { Inicio } from "../Inicio/Inicio"
 import { Portafolio } from "../Portafolio/Portafolio"
@@ -10,21 +8,19 @@ import { Skills } from "../Skills/Skills"
 const PublicComponent = () => {
     return (
         <>
-            <MantineProvider defaultColorScheme="dark">
-                <HeaderMegaMenu></HeaderMegaMenu>
-                {/* Inicio */}
-                <Inicio></Inicio>
-                {/* Certificaciones */}
-                <Certificaciones></Certificaciones>
-                {/* Skills */}
-                <Skills></Skills>
-                {/* Portafolio */}
-                <Portafolio></Portafolio>
-                {/* Contacto */}
-                <Contacto></Contacto>
-                {/* Footer */}
-                <FooterSocial></FooterSocial>
-            </MantineProvider>
+
+            <HeaderMegaMenu></HeaderMegaMenu>
+            {/* Inicio */}
+            <Inicio></Inicio>
+            {/* Certificaciones */}
+            <Certificaciones></Certificaciones>
+            {/* Skills */}
+            <Skills></Skills>
+            {/* Portafolio */}
+            <Portafolio></Portafolio>
+            {/* Contacto */}
+            <Contacto></Contacto>
+            {/* Footer */}
 
         </>
     )
