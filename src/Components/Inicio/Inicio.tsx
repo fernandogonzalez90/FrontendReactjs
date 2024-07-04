8857c6daea54a863b064ef2c3bc04f66606d6c38
fernando <fer.ema.gonzalez@gmail.com>
2024-07-04 17:59:11 -0300

    manejando imagen en inicio.tsx

--- src/Components/Inicio/Inicio.tsx
+++ src/Components/Inicio/Inicio.tsx
@@ -1,4 +1,4 @@
-import { Title, Container, Group,Avatar, ActionIcon, Flex } from '@mantine/core';
+import { Title, Container, Group, Avatar, ActionIcon, Flex } from '@mantine/core';
 import { Dots } from './Dots';
 import { useFetch } from '../useFetch';
 import classes from './Inicio.module.css';
@@ -8,37 +8,39 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 export function Inicio() {
   const { data } = useFetch<GeneralType[]>('general/')
 
+  const items = data?.map((item: GeneralType) => {
+    const imageUrl = item.imagen ? (typeof item.imagen === 'string' ? item.imagen : URL.createObjectURL(item.imagen)) : undefined;
 
-  const items = data?.map((item: GeneralType) => (
-    <Flex
-      key={item.id}
-      gap="md"
-      justify="center"
-      align="center"
-      direction="column"
-      wrap="wrap"
-    >
-      <Avatar
-        src={item.imagen}
-        size={120}
-        radius={120}
-        mx="auto"
-      />
-      <Title order={1} c="cyan">{item.subtitulo}</Title>
-      <Title order={3}>{item.titulo}</Title>
+    return (
+      <Flex
+        key={item.id}
+        gap="md"
+        justify="center"
+        align="center"
+        direction="column"
+        wrap="wrap"
+      >
+        <Avatar
+          src={imageUrl}
+          size={120}
+          radius={120}
+          mx="auto"
+        />
+        <Title order={1} c="cyan">{item.subtitulo}</Title>
+        <Title order={3}>{item.titulo}</Title>
 
-      <Group justify="center">
-        <ActionIcon variant="transparent" size="xl" color='cyan'>
-          <FaGithub />
-        </ActionIcon>
+        <Group justify="center">
+          <ActionIcon variant="transparent" size="xl" color='cyan'>
+            <FaGithub />
+          </ActionIcon>
 
-        <ActionIcon variant="transparent" size="xl" color='cyan'>
-          <FaLinkedin />
-        </ActionIcon>
-      </Group>
-    </Flex>
-
-  ));
+          <ActionIcon variant="transparent" size="xl" color='cyan'>
+            <FaLinkedin />
+          </ActionIcon>
+        </Group>
+      </Flex>
+    );
+  });
 
   return (
     <Container className={classes.wrapper} size={1400}>
