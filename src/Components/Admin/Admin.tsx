b3089ebf15e7ddb6895b0fa6b76ca99b00d141e2
fernando <fer.ema.gonzalez@gmail.com>
2024-07-04 10:55:27 -0300

    limpiamos admin

--- src/Components/Admin/Admin.tsx
+++ src/Components/Admin/Admin.tsx
@@ -3,85 +3,28 @@ import {
     Button,
     UnstyledButton,
     Text,
-    ThemeIcon,
     Divider,
     Center,
     Box,
     Burger,
     Drawer,
-    Collapse,
     ScrollArea,
     rem,
     useMantineTheme,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import {
-    IconNotification,
-    IconCode,
-    IconBook,
-    IconChartPie3,
-    IconFingerprint,
-    IconCoin,
     IconChevronDown,
 } from '@tabler/icons-react';
 import classes from './Admin.module.css';
-import { AdminGeneral } from './AdminGeneral/AdminGeneral';
+import { AdmCertificaciones } from './AdmCertificaciones/AdmCertificaciones';
 
-const mockdata = [
-    {
-        icon: IconCode,
-        title: 'Open source',
-        description: 'This Pokémon’s cry is very loud and distracting',
-    },
-    {
-        icon: IconCoin,
-        title: 'Free for everyone',
-        description: 'The fluid of Smeargle’s tail secretions changes',
-    },
-    {
-        icon: IconBook,
-        title: 'Documentation',
-        description: 'Yanma is capable of seeing 360 degrees without',
-    },
-    {
-        icon: IconFingerprint,
-        title: 'Security',
-        description: 'The shell’s rounded shape and the grooves on its.',
-    },
-    {
-        icon: IconChartPie3,
-        title: 'Analytics',
-        description: 'This Pokémon uses its flying ability to quickly chase',
-    },
-    {
-        icon: IconNotification,
-        title: 'Notifications',
-        description: 'Combusken battles with the intensely hot flames it spews',
-    },
-];
+
 
 export function Admin() {
     const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
     const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
     const theme = useMantineTheme();
-
-    const links = mockdata.map((item) => (
-        <UnstyledButton className={classes.subLink} key={item.title}>
-            <Group wrap="nowrap" align="flex-start">
-                <ThemeIcon size={34} variant="default" radius="md">
-                    <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
-                </ThemeIcon>
-                <div>
-                    <Text size="sm" fw={500}>
-                        {item.title}
-                    </Text>
-                    <Text size="xs" c="dimmed">
-                        {item.description}
-                    </Text>
-                </div>
-            </Group>
-        </UnstyledButton>
-    ));
 
     return (
         <>
@@ -135,13 +78,8 @@ export function Admin() {
                                 <Box component="span" mr={5}>
                                     Features
                                 </Box>
-                                <IconChevronDown
-                                    style={{ width: rem(16), height: rem(16) }}
-                                    color={theme.colors.blue[6]}
-                                />
                             </Center>
                         </UnstyledButton>
-                        <Collapse in={linksOpened}>{links}</Collapse>
                         <a href="#" className={classes.link}>
                             Learn
                         </a>
@@ -158,7 +96,7 @@ export function Admin() {
                     </ScrollArea>
                 </Drawer>
             </Box>
-            <AdminGeneral></AdminGeneral>
+            <AdmCertificaciones></AdmCertificaciones>
         </>
     );
 }
