227575df7b18793168dde3dc4131a445c03fec2d
fernando <fer.ema.gonzalez@gmail.com>
2024-07-04 13:12:41 -0300

    acomodando links y vistas

--- src/Components/Admin/Admin.tsx
+++ src/Components/Admin/Admin.tsx
@@ -12,8 +12,11 @@ import {
 import { useDisclosure } from '@mantine/hooks';
 import classes from './Admin.module.css';
 import { AdmCertificaciones } from './AdmCertificaciones/AdmCertificaciones';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AdmGeneral } from './AmdGeneral/AmdGeneral';
+import AdmPortafolio from './AdmPortafolio/AdmPortafolio';
+import AdmSkills from './AdmSkills/AdmSkills';
+import AdmContacto from './AdmContacto/AdmContacto';
 
 
 
@@ -21,12 +24,23 @@ export function Admin() {
     const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
     const [activeComponent, setActiveComponent] = useState<string | null>(null);
 
-    const renderComponent= () => {
-        switch (activeComponent){
+    useEffect(() => {
+        console.log('Estado activeComponent actualizado:', activeComponent);
+    }, [activeComponent]);
+
+    const renderComponent = () => {
+        console.log('Renderizando componente:', activeComponent);
+        switch (activeComponent) {
             case 'general':
                 return <AdmGeneral></AdmGeneral>;
             case 'certificaciones':
                 return <AdmCertificaciones></AdmCertificaciones>;
+            case 'portafolio':
+                return <AdmPortafolio></AdmPortafolio>;
+            case 'skills':
+                return <AdmSkills></AdmSkills>;
+            case 'contacto':
+                return <AdmContacto></AdmContacto>;
             default:
                 return <p>A ver.</p>
         }
@@ -39,25 +53,28 @@ export function Admin() {
                     <Group justify="space-between" h="100%">
                         <Text ta="center" c="cyan">Fernando Gonzalez</Text>
                         <Group h="100%" gap={0} visibleFrom="sm">
-                            <a href="" className={classes.link} onClick={()=> setActiveComponent('general')}>
+                            <a href="#" className={classes.link} onClick={() => setActiveComponent('')}>
+                                Inicio
+                            </a>
+                            <a href="#" className={classes.link} onClick={() => setActiveComponent('general')}>
                                 General
                             </a>
-                            <a href="#" className={classes.link} onClick={()=> setActiveComponent('certificaciones')}>
+                            <a href="#" className={classes.link} onClick={() => setActiveComponent('certificaciones')}>
                                 Certificaciones
                             </a>
-                            <a href="#" className={classes.link}>
+                            <a href="#" className={classes.link} onClick={() => setActiveComponent('skills')}>
                                 Skills
                             </a>
-                            <a href="#" className={classes.link}>
+                            <a href="#" className={classes.link} onClick={() => setActiveComponent('portafolio')}>
                                 Portafolio
                             </a>
-                            <a href="#" className={classes.link}>
+                            <a href="#" className={classes.link} onClick={() => setActiveComponent('contacto')}>
                                 Contacto
                             </a>
                         </Group>
 
                         <Group visibleFrom="sm">
-                            <Button variant="light" color="cyan" radius="xs">Sign up</Button>
+                            <Button variant="light" color="cyan" radius="xs">Logout</Button>
                         </Group>
 
                         <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
@@ -75,27 +92,35 @@ export function Admin() {
                 >
                     <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
                         <Divider my="sm" />
+                        <a href="#" className={classes.link} onClick={() => setActiveComponent('')}>
+                            Inicio
+                        </a>
 
-                        <a href="#" className={classes.link}>
-                            Home
+                        <a href="#" className={classes.link} onClick={() => setActiveComponent('general')}>
+                            General
                         </a>
-                        <a href="#" className={classes.link}>
-                            Learn
+                        <a href="#" className={classes.link} onClick={() => setActiveComponent('certificaciones')}>
+                            Certificaciones
                         </a>
-                        <a href="#" className={classes.link}>
-                            Academy
+                        <a href="#" className={classes.link} onClick={() => setActiveComponent('skills')}>
+                            Skills
+                        </a>
+                        <a href="#" className={classes.link} onClick={() => setActiveComponent('portafolio')}>
+                            Portafolio
+                        </a>
+                        <a href="#" className={classes.link} onClick={() => setActiveComponent('contacto')}>
+                            Contacto
                         </a>
 
                         <Divider my="sm" />
 
                         <Group justify="center" grow pb="xl" px="md">
-                            <Button variant="default">Log in</Button>
                             <Button>Exit</Button>
                         </Group>
                     </ScrollArea>
                 </Drawer>
             </Box>
-            <Box>
+            <Box mt={20}>
                 {renderComponent()}
             </Box>
         </>
