76e0e43a7834ed51b257a9cbe6fbf9a4a529322f
fernando <fer.ema.gonzalez@gmail.com>
2024-08-13 18:20:18 -0300

    fix url API

--- src/Components/AuthServices.ts
+++ src/Components/AuthServices.ts
@@ -1,7 +1,7 @@
 // authServices.tsx
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from "axios";
 
-const API_URL = 'https://backendjango.ddns.net/api/';
+const API_URL = "https://dev.gonzalezf.com.ar/api/";
 
 interface User {
   username: string;
@@ -25,7 +25,7 @@ class AuthService {
       async (config) => {
         const user = this.getCurrentUser();
         if (user && user.access) {
-          config.headers['Authorization'] = `Bearer ${user.access}`;
+          config.headers["Authorization"] = `Bearer ${user.access}`;
         }
         return config;
       },
@@ -40,7 +40,7 @@ class AuthService {
           originalRequest._retry = true;
           const newToken = await this.refreshToken();
           if (newToken) {
-            originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+            originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
             return this.api(originalRequest);
           }
         }
@@ -50,19 +50,22 @@ class AuthService {
   }
 
   async login(username: string, password: string): Promise<User> {
-    const response = await this.api.post<User>('token/', { username, password });
+    const response = await this.api.post<User>("token/", {
+      username,
+      password,
+    });
     if (response.data.access) {
-      localStorage.setItem('user', JSON.stringify(response.data));
+      localStorage.setItem("user", JSON.stringify(response.data));
     }
     return response.data;
   }
 
   logout(): void {
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
   }
 
   getCurrentUser(): User | null {
-    const userStr = localStorage.getItem('user');
+    const userStr = localStorage.getItem("user");
     if (userStr) return JSON.parse(userStr);
     return null;
   }
@@ -70,10 +73,12 @@ class AuthService {
   async refreshToken(): Promise<string | null> {
     const user = this.getCurrentUser();
     if (user && user.refresh) {
-      const response = await this.api.post<TokenResponse>('token/refresh/', { refresh: user.refresh });
+      const response = await this.api.post<TokenResponse>("token/refresh/", {
+        refresh: user.refresh,
+      });
       if (response.data.access) {
         user.access = response.data.access;
-        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem("user", JSON.stringify(user));
         return response.data.access;
       }
     }
