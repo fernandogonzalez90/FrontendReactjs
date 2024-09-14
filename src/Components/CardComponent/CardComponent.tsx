c1a20794f0154cb70973ec1283a893c4f980f34d
fernando <fer.ema.gonzalez@gmail.com>
2024-09-14 11:48:25 -0300

    del Diagl add Modal

--- src/Components/CardComponent/CardComponent.tsx
+++ src/Components/CardComponent/CardComponent.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Anchor, Button, Card, Center, Dialog, Group, Menu, Text, ThemeIcon, rem, CloseButton } from '@mantine/core'
+import { ActionIcon, Anchor, Button, Card, Center, Group, Menu, Modal, Text, ThemeIcon, rem } from '@mantine/core'
 import { CardComponentProps } from '../../Types/apiTypes'
 import iconMap from '../IconMap'
 import { PiCertificate } from 'react-icons/pi'
@@ -9,16 +9,15 @@ import { FaEye, FaGithub } from 'react-icons/fa'
 import { useState } from 'react'
 
 const CardComponent: React.FC<CardComponentProps> = ({ datos }) => {
-    const [isDescriptionDialogOpen, setIsDescriptionDialogOpen] = useState(false);
+    const [isDescriptionModalOpen, setIsDescriptionModalOpen] = useState(false);
     const [selectedDescription, setSelectedDescription] = useState<string[]>([]);
     const iconosArray = datos.iconos ? datos.iconos.split(',') : [];
 
-    const openDescriptionDialog = (description: string) => {
+    const openDescriptionModal = (description: string) => {
         const descriptionItems = description.split('|').filter(item => item.trim() !== '');
         setSelectedDescription(descriptionItems);
-        setIsDescriptionDialogOpen(true);
+        setIsDescriptionModalOpen(true);
     };
-
     return (
         <Card withBorder shadow="sm" radius="md" key={datos.id}>
             <Card.Section withBorder inheritPadding py="xs">
@@ -42,18 +41,17 @@ const CardComponent: React.FC<CardComponentProps> = ({ datos }) => {
                             </Anchor>}
 
                         {datos.tipo &&
-                            <>
-                                <Menu.Target>
-                                    <ActionIcon variant="transparent">
-                                        <HiOutlineDotsVertical color="cyan" style={{ width: rem(16), height: rem(16) }} />
-                                    </ActionIcon>
-                                </Menu.Target>
+                            <><Menu.Target>
+                                <ActionIcon variant="transparent">
+                                    <HiOutlineDotsVertical color="cyan" style={{ width: rem(16), height: rem(16) }} />
+                                </ActionIcon>
+                            </Menu.Target>
                                 <Menu.Dropdown>
                                     {datos.descripcion &&
                                         <Menu.Item leftSection={<CiCirclePlus color="cyan" style={{ width: rem(14), height: rem(14) }} />}>
                                             <Anchor target="_blank" underline="never" onClick={() => {
                                                 if (datos.descripcion) {
-                                                    openDescriptionDialog(datos.descripcion);
+                                                    openDescriptionModal(datos.descripcion);
                                                 }
                                             }}>
                                                 <Text c="cyan">Ver Mas</Text>
@@ -76,13 +74,14 @@ const CardComponent: React.FC<CardComponentProps> = ({ datos }) => {
                                             </Anchor>
                                         </Menu.Item>
                                     }
-                                </Menu.Dropdown>
-                            </>}
+                                </Menu.Dropdown></>}
                     </Menu>
                 </Group>
-            </Card.Section>
 
+            </Card.Section>
             {/* Datos Proyectos */}
+
+            {/* Caterogia del proyecto */}
             {datos.categoria &&
                 <Text mt="sm" c="dimmed" size="sm" ta="center">
                     <Text fw={700} span inherit c="cyan">
@@ -91,7 +90,7 @@ const CardComponent: React.FC<CardComponentProps> = ({ datos }) => {
                     {datos.categoria}
                     <br />
                 </Text>}
-
+            {/* Tipo del proyecto "Freelancer" */}
             {datos.tipo &&
                 <Text mt="sm" c="dimmed" size="sm" ta="center" py={4}>
                     <Text fw={700} span inherit c="cyan">
@@ -100,6 +99,8 @@ const CardComponent: React.FC<CardComponentProps> = ({ datos }) => {
                     {datos.tipo}
                 </Text>}
 
+
+            {/* Datos Certificados */}
             {datos.certificado &&
                 <Text mt="sm" c="dimmed" size="sm" ta="center" py={4}>
                     <Text fw={700} span inherit c="cyan">
@@ -113,7 +114,7 @@ const CardComponent: React.FC<CardComponentProps> = ({ datos }) => {
                 {datos.anio &&
                     <Button fullWidth variant='light' onClick={() => {
                         if (datos.descripcion) {
-                            openDescriptionDialog(datos.descripcion);
+                            openDescriptionModal(datos.descripcion);
                         }
                     }}> Temas Aprendidos</Button>
                 }
@@ -128,35 +129,20 @@ const CardComponent: React.FC<CardComponentProps> = ({ datos }) => {
                     </Group>
                 </Center>
             </Card.Section>
-            <Dialog
-                opened={isDescriptionDialogOpen}
-                onClose={() => setIsDescriptionDialogOpen(false)}
-                withCloseButton={false}
-                style={{
-                    position: 'fixed',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    maxWidth: '90vw',
-                    maxHeight: '90vh',
-                    padding: '1rem',
-                    overflowY: 'auto',
-                    backgroundColor: 'white',
-                    borderRadius: '10px',
-                    boxShadow: '0px 10px 30px rgba(0, 0, 0, 0.1)'
-                }}
+            <Modal
+                size={"lg"}
+                centered
+                opened={isDescriptionModalOpen}
+                onClose={() => setIsDescriptionModalOpen(false)}
             >
-                <Group justify="flex-end">
-                    <CloseButton onClick={() => setIsDescriptionDialogOpen(false)} />
-                </Group>
                 <ul>
                     {selectedDescription.map((item, index) => (
                         <li key={index}>{item.trim()}</li>
                     ))}
                 </ul>
-            </Dialog>
+            </Modal>
         </Card>
     )
 }
 
-export default CardComponent;
+export default CardComponent
