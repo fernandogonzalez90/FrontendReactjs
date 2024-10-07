1cf789dc4d149ac1b561681d580f58dfc144e758
fernando <fer.ema.gonzalez@gmail.com>
2024-10-07 18:38:20 -0300

    chore: comment functions

--- src/Components/Servicios/Servicios.tsx
+++ src/Components/Servicios/Servicios.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Container, Divider, Title, Text, Paper } from "@mantine/core";
+import { Container, Divider, Title, Text, Paper, Center } from "@mantine/core";
 import { HeaderMegaMenu } from "../HeaderMegaMenu/HeaderMegaMenu";
 
 interface SectionTitleProps {
@@ -12,15 +12,15 @@ const SectionTitle: React.FC<SectionTitleProps> = ({ children }) => (
     </Title>
 );
 
-interface SectionTextProps {
-    children: ReactNode;
-}
+// interface SectionTextProps {
+//     children: ReactNode;
+// }
 
-const SectionText: React.FC<SectionTextProps> = ({ children }) => (
-    <Text size="lg" ta="center" c="dimmed" mb="lg">
-        {children}
-    </Text>
-);
+// const SectionText: React.FC<SectionTextProps> = ({ children }) => (
+//     <Text size="lg" ta="center" c="dimmed" mb="lg">
+//         {children}
+//     </Text>
+// );
 
 interface BulletPointProps {
     children: ReactNode;
@@ -37,32 +37,37 @@ export const Servicios: React.FC = () => {
         <>
             <HeaderMegaMenu />
             <Container size="lg" id="portafolio">
-                <Divider my="md" color="cyan.3" />
+                {/* <Divider my="md" color="cyan.3" /> */}
                 <Title order={2} ta="center" c="cyan" mt="sm" pt="lg">
                     Servicios Freelancer
                 </Title>
-                <SectionText>
-                    En esta sección hay tanto proyectos personales como trabajos realizados como Freelancer.
-                </SectionText>
+                <Center>
+                    <Text ta="center" c="dimmed" size="lg" w={500}>
+                        Estoy comprometido a ofrecer soluciones eficientes y de alta calidad para satisfacer las necesidades de mis clientes en cada proyecto que emprendo.
+                    </Text>
+                </Center>
+                <br />
 
-                <Paper p="lg" shadow="xs" mb="lg">
+                <Paper p="lg" bg="dark" shadow="xs" mb="lg">
                     <SectionTitle>Perfil Profesional</SectionTitle>
+                    <Divider my="md" color="cyan.3" />
                     <Text>
                         Soy desarrollador Fullstack con orientación DevOps Engineer, estando dentro de mi stack:
                     </Text>
-                    <Text w={500} mt="xs">
-                        Frontend: JavaScript, TypeScript, ReactJS, Bootstrap, Mantine UI y Material UI
+                    <Text mt="xs">
+                        <b>Frontend:</b> JavaScript, TypeScript, ReactJS, Bootstrap, Mantine UI y Material UI
                     </Text>
-                    <Text w={500} mt="xs">
-                        Backend: Python, Django, FastAPI, SqlAlchemy, PostgreSQL, SQLite3
+                    <Text mt="xs">
+                        <b>Backend:</b> Python, Django, FastAPI, SqlAlchemy, PostgreSQL, SQLite3
                     </Text>
-                    <Text w={500} mt="xs">
-                        DevOps: AWS, Terraform, Docker, Git, Jenkins, Ansible y Bash
+                    <Text mt="xs">
+                        <b>DevOps:</b> AWS, Terraform, Docker, Git, Jenkins, Ansible y Bash
                     </Text>
                 </Paper>
 
-                <Paper p="lg" shadow="xs" mb="lg">
+                <Paper p="lg" bg="dark" shadow="xs" mb="lg">
                     <SectionTitle>Áreas de Especialización</SectionTitle>
+                    <Divider my="md" color="cyan.3" />
                     <BulletPoint>Automatización de formularios para agilizar procesos.</BulletPoint>
                     <BulletPoint>Extracción y transformación de datos a partir de archivos para su posterior exportación.</BulletPoint>
                     <BulletPoint>Implementación de atajos de teclado personalizados para optimizar tareas específicas.</BulletPoint>
@@ -72,18 +77,16 @@ export const Servicios: React.FC = () => {
                     <BulletPoint>Manipulación de imágenes para la extracción de información relevante.</BulletPoint>
                 </Paper>
 
-                <Paper p="lg" shadow="xs" mb="lg">
+                <Paper p="lg" bg="dark" shadow="xs" mb="lg">
                     <SectionTitle>Proyectos de Front-End</SectionTitle>
+                    <Divider my="md" color="cyan.3" />
                     <BulletPoint>Diseño y desarrollo de Landing Pages atractivas y funcionales.</BulletPoint>
                     <BulletPoint>Creación de aplicaciones web modernas y responsivas.</BulletPoint>
                     <BulletPoint>Integración con APIs externas para la obtención y presentación de datos.</BulletPoint>
                     <BulletPoint>Desarrollo de sitios web con secciones administrativas para la gestión de contenidos.</BulletPoint>
                 </Paper>
 
-                <Text ta="center" size="lg" w={500}>
-                    Estoy comprometido a ofrecer soluciones eficientes y de alta calidad para satisfacer las necesidades de mis clientes en cada proyecto que emprendo.
-                </Text>
-            </Container>
+            </Container >
         </>
     );
 };
